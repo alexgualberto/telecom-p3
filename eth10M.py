c8bb4f95dd3e65014c9a40a5ed657890ae714a15
Alexandre Gualberto <alexandre.santos.gualberto@gmail.com>
2024-09-03 20:37:19 -0300

    Update eth10M.py

--- eth10M.py
+++ eth10M.py
@@ -1,8 +1,8 @@
 def decode(sig):
     estado_atual = 0
-    quadro_ethernet = []
-    ultimo_simbolo = None
-    contador_de_bits = 0
+    quadro_ethernet = list()
+    ultimo_simbolo = 0
+    contador_bits = 0
     byte_atual = 0
 
     for simbolo in extrair_simbolos(sig):
@@ -11,28 +11,26 @@ def decode(sig):
                 estado_atual = 1
             ultimo_simbolo = simbolo
         elif estado_atual == 1:
-            byte_atual |= (simbolo << contador_de_bits)
-            contador_de_bits += 1
+            byte_atual = byte_atual | (simbolo << contador_bits)
             
-            if contador_de_bits == 8:
+            if contador_bits == 7:
                 quadro_ethernet.append(byte_atual)
                 byte_atual = 0
-                contador_de_bits = 0
-
+            contador_bits = (contador_bits + 1) % 8
     return bytes(quadro_ethernet)
 
 def extrair_simbolos(amostras):
-    contador_de_posicao = 1
+    posicao = 1
     nivel_atual = amostras[0]
 
     for amostra in amostras[1:]:
         if amostra == nivel_atual:
-            contador_de_posicao += 1
+            posicao += 1
         else:
-            if contador_de_posicao >= 20:
+            if posicao >= 20:
                 # Canal ocioso detectado, encerra a transmissão
                 break
-            if contador_de_posicao >= 6:
+            if posicao >= 6:
                 yield 1 - nivel_atual  # Inversão do símbolo
-            contador_de_posicao = 1
+            posicao = 1
             nivel_atual = amostra
